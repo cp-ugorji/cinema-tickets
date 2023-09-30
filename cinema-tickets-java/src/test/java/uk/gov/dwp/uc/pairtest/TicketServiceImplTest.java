4520a81c0542622f3bbc17b996da78d7ade9177d
Chukwudi Ugorji <cp.ugorji@gmail.com>
2023-09-30 07:39:34 +0100

    using TDD approach to find a solution

---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,18 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.Before;
+import static org.mockito.Mockito.*;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+
+public class TicketServiceImplTest {
+    private TicketServiceImpl ticketService;
+    private TicketPaymentService paymentService;
+    private SeatReservationService reservationService;
+
+    @Before
+    public void setup() {
+        paymentService = mock(TicketPaymentService.class);
+        reservationService = mock(SeatReservationService.class);
+    }
+}
