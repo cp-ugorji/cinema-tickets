abb35ac8c33099e2d4664d5c8dbabca64b9f9efc
Chukwudi Ugorji <cp.ugorji@gmail.com>
2023-09-30 07:52:07 +0100

    failing unit test to check account id validity

--- cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -2,8 +2,12 @@ package uk.gov.dwp.uc.pairtest;
 
 import org.junit.Before;
 import static org.mockito.Mockito.*;
+
+import org.junit.Test;
 import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImplTest {
     private TicketServiceImpl ticketService;
@@ -15,5 +19,10 @@ public class TicketServiceImplTest {
         paymentService = mock(TicketPaymentService.class);
         reservationService = mock(SeatReservationService.class);
         ticketService = new TicketServiceImpl(paymentService, reservationService);
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void shouldThrowExceptionForInvalidAccountId() {
+        ticketService.purchaseTickets(0L, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
     }
 }
